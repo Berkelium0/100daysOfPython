c2106f72200e9dbb2020dc51b2ec35ea7a9c3570
Berkelium0 <berkeuysal0@gmail.com>
2022-05-04 11:05:21 +0300

    Initial setup 3

--- Day08 - Caesar Cipher/main.py
+++ Day08 - Caesar Cipher/main.py
@@ -3,7 +3,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def  caesar(start_text, shift_amount, cipher_direction):
+def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
