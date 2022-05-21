a14c12a4e3ba38fb7260c2eff8b914a261e3a65b
Berkelium0 <berkeuysal0@gmail.com>
2022-05-21 17:02:42 +0300

    Day 20-21

---
+++ Day 20-21 - Snake Game/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_section(position)
+
+    def add_section(self, position):
+        new_t = Turtle("square")
+        new_t.color("white")
+        new_t.penup()
+        new_t.goto(position)
+        self.segments.append(new_t)
+
+    def extend(self):
+        self.add_section(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_num].goto((self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor()))
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
