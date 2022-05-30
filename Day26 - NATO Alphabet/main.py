c5a35f0e2fce7a5f4c2a3f34e3531b71e20f9b46
Berkelium0 <berkeuysal0@gmail.com>
2022-05-30 17:14:01 +0300

    Day 30 - Learned about exception handling and updated Day26's code to have better error detection

--- Day26 - NATO Alphabet/main.py
+++ Day26 - NATO Alphabet/main.py
@@ -6,8 +6,18 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-# Create a list of the phonetic code words from a word that the user inputs.
-entered_word = input("Enter a word: ").upper()
 
-result = [nato_dict[letter] for letter in entered_word]
-print(result)
+# Create a list of the phonetic code words from a word that the user inputs.
+
+def enter():
+    entered_word = input("Enter a word: ").upper()
+    try:
+        result = [nato_dict[letter] for letter in entered_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        enter()
+    else:
+        print(result)
+
+
+enter()
